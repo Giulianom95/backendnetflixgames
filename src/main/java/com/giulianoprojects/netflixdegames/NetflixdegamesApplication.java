2af08a2ba26994072f6acc93fb9cee0c69c38a28
Giuliano mario <132069668+Giulianom95@users.noreply.github.com>
2025-08-21 21:28:12 -0300

    Primeiro commit

---
+++ src/main/java/com/giulianoprojects/netflixdegames/NetflixdegamesApplication.java
@@ -0,0 +1,13 @@
+package com.giulianoprojects.netflixdegames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetflixdegamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetflixdegamesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giulianoprojects/netflixdegames/NetflixdegamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giulianoprojects.netflixdegames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixdegamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
